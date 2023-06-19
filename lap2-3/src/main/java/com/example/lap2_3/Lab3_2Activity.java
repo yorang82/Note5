988487e93809e425e0bb7e5d94a5993635babea5
yorang82 <136775566+yorang82@users.noreply.github.com>
2023-06-19 11:36:14 +0900

    Initial commit

---
+++ lap2-3/src/main/java/com/example/lap2_3/Lab3_2Activity.java
@@ -0,0 +1,14 @@
+package com.example.lap2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab3_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab3_2);
+    }
+}
