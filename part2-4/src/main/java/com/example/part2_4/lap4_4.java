662c5960e73d00cfd8d9cfad38bcf028b6369531
yorang82 <136775566+yorang82@users.noreply.github.com>
2023-06-26 17:42:30 +0900

    part 2-4     lap 4-2     lap 4-3     lap 4-4 ing

---
+++ part2-4/src/main/java/com/example/part2_4/lap4_4.java
@@ -0,0 +1,14 @@
+package com.example.part2_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lap4_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lap4_4);
+    }
+}
