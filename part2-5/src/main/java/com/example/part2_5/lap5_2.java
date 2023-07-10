cf2ccfda64ef9b27f9b08c6f30c3af856dfbff84
yorang82 <136775566+yorang82@users.noreply.github.com>
2023-07-10 17:45:50 +0900

    part 2-5      lap 5-2

---
+++ part2-5/src/main/java/com/example/part2_5/lap5_2.java
@@ -0,0 +1,15 @@
+package com.example.part2_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lap5_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lap5_2);
+    }
+}
+
