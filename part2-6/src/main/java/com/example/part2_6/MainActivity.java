24721f94f70a47e5a77137563c953eb529c01eda
yorang82 <136775566+yorang82@users.noreply.github.com>
2023-07-12 12:10:12 +0900

    part 2-7 start

--- part2-6/src/main/java/com/example/part2_6/MainActivity.java
+++ part2-6/src/main/java/com/example/part2_6/MainActivity.java
@@ -67,13 +67,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     float initX;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction()==MotionEvent.ACTION_DOWN){
-            initX=event.getRawX();
-        } else if (event.getAction()==MotionEvent.ACTION_UP ) {
-            float diffX=initX-event.getRawX();
-            if (diffX>30) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            initX = event.getRawX();
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            float diffX = initX - event.getRawX();
+            if (diffX > 30) {
                 showToast("왼쪽으로 화면을 밀었습니다.");
             } else if (diffX < -30) {
                 showToast("오른쪽으로 화면을 밀었습니다.");
@@ -86,18 +87,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode==KeyEvent.KEYCODE_BACK){
-            if (System.currentTimeMillis() - initTime > 3000){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - initTime > 3000) {
                 showToast("종료할려면 한번 더 누르세요.");
-                initTime=System.currentTimeMillis();
-            }else {
+                initTime = System.currentTimeMillis();
+            } else {
                 finish();
             }
             return true;
         }
-
         return super.onKeyDown(keyCode, event);
-
     }
     //    @Override
 //    public void onPointerCaptureChanged(boolean hasCapture) {
