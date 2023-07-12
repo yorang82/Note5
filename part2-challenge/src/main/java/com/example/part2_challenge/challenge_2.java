60bdd6955828ef704ee2b82408f9d70740db9d1e
Yorang82_home <yorang82@nate.com>
2023-07-12 22:43:41 +0900

    part 2-challenge        challenge_2 start

---
+++ part2-challenge/src/main/java/com/example/part2_challenge/challenge_2.java
@@ -0,0 +1,14 @@
+package com.example.part2_challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class challenge_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_2);
+    }
+}
