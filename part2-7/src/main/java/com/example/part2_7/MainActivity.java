7b9404790ff5bab405098c324944e1f644bf55bb
yorang82 <136775566+yorang82@users.noreply.github.com>
2023-07-12 15:11:07 +0900

    part 2-7    lab 7-1 end

--- part2-7/src/main/java/com/example/part2_7/MainActivity.java
+++ part2-7/src/main/java/com/example/part2_7/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.part2_7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=(ImageView) findViewById(R.id.img);
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.in);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation anim = AnimationUtils.loadAnimation(MainActivity.this,R.anim.move);
+                anim.setFillAfter(true);
+                imageView.startAnimation(anim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageView.startAnimation(anim);
     }
 }
